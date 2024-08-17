6d89a92acfbfa35456f64c423a1543ed5af9c749
Hadi <hadi.github.acc@gmail.com>
2024-08-17 22:17:28 +0300

    dummy usage of types

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,5 +1,14 @@
 //eslint-disable-next-line
+
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+// Dummy usage to bypass unused import error
+const PaletteTypes = (): Palette | PaletteColor | null => {
+    return null;
+};
+
+// Call the dummy function
+PaletteTypes();
+
 
 declare module "@mui/material/styles/createPalette" {
     interface PaletteColor{
